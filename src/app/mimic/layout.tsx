ca9a95bdb8dd10e04d4f66fb118ecb96a2f58087
Lucienwooo <101056203+Lucienwooo@users.noreply.github.com>
2026-05-06 17:06:20 +0800

    .

--- src/app/mimic/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function MimicLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div style={{ width: '100%' }}>
-            {children}
-        </div>
-    );
-}
